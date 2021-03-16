a56cf2bbb82366f7504163c3961695580b694599
Ramprasad273 <c.ramprasad273@gmail.com>
2021-03-17 01:27:45 +0530

    Added config details

--- Data-Streaming/Kakfa_projects/Kafka_101/kafka_configurations.py
+++ Data-Streaming/Kakfa_projects/Kafka_101/kafka_configurations.py
@@ -5,9 +5,10 @@ import logging
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
+
 def get_producer_config():
     logging.info("Creating producer object ..")
-
+    # The Producer object requires the Kafka server, Json serializer
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                              value_serializer=lambda x:
                              json.dumps(x).encode('utf-8'))
@@ -17,6 +18,8 @@ def get_producer_config():
 
 def get_consumer_events(topic):
     logging.info("Creating Consumer Object ..")
+    # The consumer object contains the topic name, json deserializer,Kafka servers
+    # and kafka time out in ms, Stops the iteration if no message after 1 sec
     consumer = KafkaConsumer(topic,
                              group_id='my-group',
                              value_deserializer=lambda m: json.loads(m.decode('ascii')),
