f1a47d82f7b1a7dac36c8b0d8343f3c0db46dd7a
c.ramprasad <c.ramprasad273@gmail.com>
2026-01-31 16:27:02 +0530

    code cleanup

--- de_projects/de_sandbox/docker/airflow/plugins/monitoring.py
+++
@@ -1,7 +0,0 @@
-
-def on_failure_callback(context):
-    """
-    This function will be called on task failure.
-    """
-    print(f"Task {context['task_instance_key_str']} failed.")
-    # In a real-world scenario, you would send an alert to a Slack channel or other monitoring tool.
