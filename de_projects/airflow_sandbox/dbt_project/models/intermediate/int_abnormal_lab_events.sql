a6a3af0c4f46ff9a46b6851cdb49501686158517
c.ramprasad <c.ramprasad273@gmail.com>
2026-09-20 00:07:16 +0530

    feat: add int_abnormal_lab_events dbt model to aggregate abnormal lab counts per patient

--- de_projects/airflow_sandbox/dbt_project/models/intermediate/int_abnormal_lab_events.sql
+++ de_projects/airflow_sandbox/dbt_project/models/intermediate/int_abnormal_lab_events.sql
@@ -3,7 +3,7 @@
 SELECT
     patient_id,
     COUNT(*) AS abnormal_lab_count,
-    MAX(resulted_at) AS last_abnormal_at
+    -- MAX(resulted_at) AS last_abnormal_at
 FROM {{ ref('stg_lab_results') }}
 WHERE is_abnormal = TRUE
 GROUP BY patient_id
