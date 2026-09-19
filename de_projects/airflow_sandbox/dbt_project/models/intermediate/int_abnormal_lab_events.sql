36f695e554d231cf7a89ef5bcc995be54d39725a
c.ramprasad <c.ramprasad273@gmail.com>
2026-09-20 00:55:27 +0530

    feat: add int_abnormal_lab_events intermediate dbt model

--- de_projects/airflow_sandbox/dbt_project/models/intermediate/int_abnormal_lab_events.sql
+++ de_projects/airflow_sandbox/dbt_project/models/intermediate/int_abnormal_lab_events.sql
@@ -2,7 +2,7 @@
 
 SELECT
     patient_id,
-    COUNT(*) AS abnormal_lab_count,
+    -- COUNT(*) AS abnormal_lab_count,
     MAX(resulted_at) AS last_abnormal_at
 FROM {{ ref('stg_lab_results') }}
 WHERE is_abnormal = TRUE
